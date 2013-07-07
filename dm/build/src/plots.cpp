bb93261d85d8a69c3ba4188db1824d4de7b719a1
Andrew Rasmussen <andyras@gmail.com>
2013-07-07 11:43:15 -0500

    compacted and standardized plotting code

--- dm/build/src/plots.cpp
+++ dm/build/src/plots.cpp
@@ -32,7 +32,7 @@ void plotPopulations(char * fileName, struct PARAMETERS * p) {
  o << "set style increment user" << std::endl;
  o << std::endl;
  o << "set terminal pdfcairo enhanced size 5,5 lw 2 font 'Arial-Bold,12'" << std::endl;
- o << "set output 'populations.pdf'" << std::endl;
+ o << "set output 'figures/populations.pdf'" << std::endl;
  o << std::endl;
  o << "stats './outs/tkprob.out' nooutput" << std::endl;
  o << "set xrange [STATS_min_x:STATS_max_x]" << std::endl;
@@ -77,7 +77,7 @@ void plotPopulations(char * fileName, struct PARAMETERS * p) {
  o << "unset multiplot" << std::endl;
  o << std::endl;
  o << "set terminal pdfcairo enhanced size 5,3 lw 2 font 'Arial-Bold,12' dashed rounded" << std::endl;
- o << "set output 'populationsTogether.pdf'" << std::endl;
+ o << "set output 'figures/populationsTogether.pdf'" << std::endl;
  o << std::endl;
  o << "set key tmargin right" << std::endl;
  o << "set size 1,1" << std::endl;
@@ -102,50 +102,51 @@ void plotKProbs(char * fileName, struct PARAMETERS * p) {
 #ifdef DEBUG_PLOT
  std::cout << "\nMaking kprobs.plt" << std::endl;
 #endif
- std::ofstream output(fileName);
+ std::ofstream o(fileName);
 
- output << "#!/usr/bin/env gnuplot" << std::endl
-        << std::endl
-        << "set terminal pdfcairo size 5,3 font 'Arial-Bold,12'" << std::endl
-        << std::endl
-        << "set output '/dev/null'" << std::endl
-        << "plot '<cut -d \" \" -f 2- ./outs/kprobs.out' u ($2*"
-	<< p->tout << "/" << p->numOutputSteps << "):($1*(" 
-	<< p->kBandTop << "-" << p->kBandEdge << ")/("
-	<< p->Nk << "-1)):3 matrix with image" << std::endl
-        << std::endl
-        << "set output 'figures/kprobs.pdf'" << std::endl
-        << "set title 'Electron probability density in bulk conduction band'" << std::endl
-        << "unset key " << std::endl
-        //<< "unset colorbox" << std::endl
-        << "set border 0" << std::endl
-        << "set tics scale 0" << std::endl
-        << "set ylabel 'Energy above band edge (a.u.)'" << std::endl
-        << "set xlabel 'Time (a.u.)'" << std::endl
-        << "set xrange [GPVAL_DATA_X_MIN:GPVAL_DATA_X_MAX]" << std::endl
-        << "set yrange [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]" << std::endl
-        << std::endl
-        << "set palette defined(\\" << std::endl
-        << "0.0    0.8667  0.8667  0.8667,\\" << std::endl
-        << "0.0625 0.8980  0.8471  0.8196,\\" << std::endl
-        << "0.125  0.9255  0.8275  0.7725,\\" << std::endl
-        << "0.1875 0.9451  0.8000  0.7255,\\" << std::endl
-        << "0.25   0.9608  0.7686  0.6784,\\" << std::endl
-        << "0.3125 0.9686  0.7333  0.6275,\\" << std::endl
-        << "0.375  0.9686  0.6941  0.5804,\\" << std::endl
-        << "0.4375 0.9686  0.6510  0.5294,\\" << std::endl
-        << "0.5    0.9569  0.6039  0.4824,\\" << std::endl
-        << "0.5625 0.9451  0.5529  0.4353,\\" << std::endl
-        << "0.625  0.9255  0.4980  0.3882,\\" << std::endl
-        << "0.6875 0.8980  0.4392  0.3451,\\" << std::endl
-        << "0.75   0.8706  0.3765  0.3020,\\" << std::endl
-        << "0.8125 0.8353  0.3137  0.2588,\\" << std::endl
-        << "0.875  0.7961  0.2431  0.2196,\\" << std::endl
-        << "0.9375 0.7529  0.1569  0.1843,\\" << std::endl
-        << "1      0.7059  0.0157  0.1490\\" << std::endl
-        << ")" << std::endl
-        << std::endl
-        << "replot" << std::endl;
+ o << "#!/usr/bin/env gnuplot" << std::endl;
+ o << std::endl;
+ o << "set terminal pdfcairo size 5,3 font 'Arial-Bold,12'" << std::endl;
+ o << std::endl;
+ o << "set output '/dev/null'" << std::endl;
+ o << "plot '<cut -d \" \" -f 2- ./outs/kprobs.out' u ($2*"
+   << p->tout << "/" << p->numOutputSteps << "):($1*(" 
+   << p->kBandTop << "-" << p->kBandEdge << ")/(";
+ o << p->Nk << "-1)):3 matrix with image" << std::endl;
+ o << std::endl;
+ o << "set output 'figures/kprobs.pdf'" << std::endl;
+ o << "set title 'Electron probability density in bulk conduction band'" << std::endl;
+ o << "unset key " << std::endl;
+ //o << "unset colorbox" << std::endl;
+ o << "set border 0" << std::endl;
+ o << "set tics scale 0" << std::endl;
+ o << "set ylabel 'Energy above band edge (a.u.)'" << std::endl;
+ o << "set xlabel 'Time (a.u.)'" << std::endl;
+ o << "set xrange [GPVAL_DATA_X_MIN:GPVAL_DATA_X_MAX]" << std::endl;
+ o << "set yrange [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]" << std::endl;
+ o << std::endl;
+ o << "set palette defined(\\" << std::endl;
+ o << "0.0    0.8667  0.8667  0.8667,\\" << std::endl;
+ o << "0.0625 0.8980  0.8471  0.8196,\\" << std::endl;
+ o << "0.125  0.9255  0.8275  0.7725,\\" << std::endl;
+ o << "0.1875 0.9451  0.8000  0.7255,\\" << std::endl;
+ o << "0.25   0.9608  0.7686  0.6784,\\" << std::endl;
+ o << "0.3125 0.9686  0.7333  0.6275,\\" << std::endl;
+ o << "0.375  0.9686  0.6941  0.5804,\\" << std::endl;
+ o << "0.4375 0.9686  0.6510  0.5294,\\" << std::endl;
+ o << "0.5    0.9569  0.6039  0.4824,\\" << std::endl;
+ o << "0.5625 0.9451  0.5529  0.4353,\\" << std::endl;
+ o << "0.625  0.9255  0.4980  0.3882,\\" << std::endl;
+ o << "0.6875 0.8980  0.4392  0.3451,\\" << std::endl;
+ o << "0.75   0.8706  0.3765  0.3020,\\" << std::endl;
+ o << "0.8125 0.8353  0.3137  0.2588,\\" << std::endl;
+ o << "0.875  0.7961  0.2431  0.2196,\\" << std::endl;
+ o << "0.9375 0.7529  0.1569  0.1843,\\" << std::endl;
+ o << "1      0.7059  0.0157  0.1490\\" << std::endl;
+ o << ")" << std::endl;
+ o << std::endl;
+ o << "replot" << std::endl;
+
  return;
 }
 
@@ -154,47 +155,48 @@ void plotCProbs(struct PARAMETERS * p) {
 #ifdef DEBUG_PLOT
  std::cout << "\nMaking cprobs.plt" << std::endl;
 #endif
- std::ofstream output("cprobs.plt");
- output << "#!/usr/bin/env gnuplot" << std::endl
-        << std::endl
-        << "set terminal pdfcairo size 5,3 font 'Arial-Bold,12'" << std::endl
-        << std::endl
-        << "set output '/dev/null'" << std::endl
-        << "plot '<cut -d \" \" -f 2- ./outs/cprobs.out' u ($2*"
-	<< p->tout << "/" << p->numOutputSteps << "):($1):3 matrix with image" << std::endl
-        << std::endl
-        << "set output 'figures/cprobs.pdf'" << std::endl
-        << "set title 'Electron probability density in QD'" << std::endl
-        << "unset key " << std::endl
-        //<< "unset colorbox" << std::endl
-        << "set border 0" << std::endl
-        << "set tics scale 0" << std::endl
-        << "set ylabel 'State (index above band edge)'" << std::endl
-        << "set xlabel 'Time (a.u.)'" << std::endl
-        << "set xrange [GPVAL_DATA_X_MIN:GPVAL_DATA_X_MAX]" << std::endl
-        << "set yrange [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]" << std::endl
-        << std::endl
-        << "set palette defined(\\" << std::endl
-        << "0.0    0.8667  0.8667  0.8667,\\" << std::endl
-        << "0.0625 0.8980  0.8471  0.8196,\\" << std::endl
-        << "0.125  0.9255  0.8275  0.7725,\\" << std::endl
-        << "0.1875 0.9451  0.8000  0.7255,\\" << std::endl
-        << "0.25   0.9608  0.7686  0.6784,\\" << std::endl
-        << "0.3125 0.9686  0.7333  0.6275,\\" << std::endl
-        << "0.375  0.9686  0.6941  0.5804,\\" << std::endl
-        << "0.4375 0.9686  0.6510  0.5294,\\" << std::endl
-        << "0.5    0.9569  0.6039  0.4824,\\" << std::endl
-        << "0.5625 0.9451  0.5529  0.4353,\\" << std::endl
-        << "0.625  0.9255  0.4980  0.3882,\\" << std::endl
-        << "0.6875 0.8980  0.4392  0.3451,\\" << std::endl
-        << "0.75   0.8706  0.3765  0.3020,\\" << std::endl
-        << "0.8125 0.8353  0.3137  0.2588,\\" << std::endl
-        << "0.875  0.7961  0.2431  0.2196,\\" << std::endl
-        << "0.9375 0.7529  0.1569  0.1843,\\" << std::endl
-        << "1      0.7059  0.0157  0.1490\\" << std::endl
-        << ")" << std::endl
-        << std::endl
-        << "replot" << std::endl;
+ std::ofstream o("cprobs.plt");
+
+ o << "#!/usr/bin/env gnuplot" << std::endl;
+ o << std::endl;
+ o << "set terminal pdfcairo size 5,3 font 'Arial-Bold,12'" << std::endl;
+ o << std::endl;
+ o << "set output '/dev/null'" << std::endl;
+ o << "plot '<cut -d \" \" -f 2- ./outs/cprobs.out' u ($2*"
+   << p->tout << "/" << p->numOutputSteps << "):($1):3 matrix with image" << std::endl;
+ o << std::endl;
+ o << "set output 'figures/cprobs.pdf'" << std::endl;
+ o << "set title 'Electron probability density in QD'" << std::endl;
+ o << "unset key " << std::endl;
+ //o << "unset colorbox" << std::endl
+ o << "set border 0" << std::endl;
+ o << "set tics scale 0" << std::endl;
+ o << "set ylabel 'State (index above band edge)'" << std::endl;
+ o << "set xlabel 'Time (a.u.)'" << std::endl;
+ o << "set xrange [GPVAL_DATA_X_MIN:GPVAL_DATA_X_MAX]" << std::endl;
+ o << "set yrange [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]" << std::endl;
+ o << std::endl;
+ o << "set palette defined(\\" << std::endl;
+ o << "0.0    0.8667  0.8667  0.8667,\\" << std::endl;
+ o << "0.0625 0.8980  0.8471  0.8196,\\" << std::endl;
+ o << "0.125  0.9255  0.8275  0.7725,\\" << std::endl;
+ o << "0.1875 0.9451  0.8000  0.7255,\\" << std::endl;
+ o << "0.25   0.9608  0.7686  0.6784,\\" << std::endl;
+ o << "0.3125 0.9686  0.7333  0.6275,\\" << std::endl;
+ o << "0.375  0.9686  0.6941  0.5804,\\" << std::endl;
+ o << "0.4375 0.9686  0.6510  0.5294,\\" << std::endl;
+ o << "0.5    0.9569  0.6039  0.4824,\\" << std::endl;
+ o << "0.5625 0.9451  0.5529  0.4353,\\" << std::endl;
+ o << "0.625  0.9255  0.4980  0.3882,\\" << std::endl;
+ o << "0.6875 0.8980  0.4392  0.3451,\\" << std::endl;
+ o << "0.75   0.8706  0.3765  0.3020,\\" << std::endl;
+ o << "0.8125 0.8353  0.3137  0.2588,\\" << std::endl;
+ o << "0.875  0.7961  0.2431  0.2196,\\" << std::endl;
+ o << "0.9375 0.7529  0.1569  0.1843,\\" << std::endl;
+ o << "1      0.7059  0.0157  0.1490\\" << std::endl;
+ o << ")" << std::endl;
+ o << std::endl;
+ o << "replot" << std::endl;
  /*
  output << "#!/usr/bin/env gnuplot\n\n"
  << "reset\n"
