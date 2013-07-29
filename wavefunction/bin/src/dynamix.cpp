527e6af92ae69ed1880d808d455264c887a4db18
Andrew Rasmussen <andyras@gmail.com>
2013-07-29 15:01:32 -0500

    small changes

--- wavefunction/bin/src/dynamix.cpp
+++ wavefunction/bin/src/dynamix.cpp
@@ -20,7 +20,7 @@
 #include "numerical.h"
 
 /* DEBUG compiler flag: turn on to generate basic debug outputs.         */
-#define DEBUG
+//#define DEBUG
 // DEBUG2 flag: turn on for more numerical output
 //#define DEBUG2
 /* DANGER! Only turn on DEBUGf for small test runs, otherwise output is       */
