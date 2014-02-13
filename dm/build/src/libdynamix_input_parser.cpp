f1ef32dd2a6382e592c7b6bdddd87a2c62ecda9e
Andrew Rasmussen <andyras@gmail.com>
2014-02-13 16:57:01 -0600

    added parameter to dynamically calculate Fermi level in kinetic model

--- dm/build/src/libdynamix_input_parser.cpp
+++ dm/build/src/libdynamix_input_parser.cpp
@@ -142,6 +142,7 @@ void assignParams(std::string inputFile, struct PARAMETERS * p) {
     else if (input_param == "coherent") { p->coherent = atoi(param_val.c_str()); }
     else if (input_param == "analytical") { p->analytical = atoi(param_val.c_str()); }
     else if (input_param == "kinetic") { p->kinetic = atoi(param_val.c_str()); }
+    else if (input_param == "dynamicMu") { p->dynamicMu = atoi(param_val.c_str()); }
     else if (input_param == "rta") { p->rta = atoi(param_val.c_str()); }
     else if (input_param == "rtaQD") { p->rtaQD = atoi(param_val.c_str()); }
     else if (input_param == "dephasing") { p->dephasing = atoi(param_val.c_str()); }
@@ -214,6 +215,7 @@ void assignParams(std::string inputFile, struct PARAMETERS * p) {
   std::cout << "coherent is " << p->coherent << std::endl;
   std::cout << "analytical is " << p->analytical << std::endl;
   std::cout << "kinetic is " << p->kinetic << std::endl;
+  std::cout << "dynamicMu is " << p->dynamicMu << std::endl;
   std::cout << "rta is " << p->rta << std::endl;
   std::cout << "rtaQD is " << p->rtaQD << std::endl;
   std::cout << "dephasing is " << p->dephasing << std::endl;
