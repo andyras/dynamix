dbd957322700443d018221ff3556c2392fa9bcef
Andrew Rasmussen <andyras@gmail.com>
2014-02-25 20:31:53 -0600

    hud.py now takes argument for directory containing output files

--- dm/tools/hud.py
+++ dm/tools/hud.py
@@ -2,175 +2,198 @@
 
 debug = False
 
-import matplotlib as mpl
-# optionally set up mpl things
-#mpl.use("TkAgg")
-# kill the toolbar
-mpl.rcParams['toolbar'] = 'None'
+import argparse
+import os
 
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.animation as animation
+parser = argparse.ArgumentParser(description='This script displays population dynamics between two sets of electronic states')
+parser.add_argument('--dir', '-d', help='directory containing job outputs', type=str, metavar='<job dir>', default='')
 
-ii = 0
-def getIdx():
-    iiMax = numSteps
-    global ii
+args = parser.parse_args()
+
+class cd:
+    '''
+    Context manager for changing the current working directory
+    '''
+    def __init__(self, newPath):
+        self.newPath = newPath
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+with cd(os.path.expanduser(args.dir)):
+    import matplotlib as mpl
+    # optionally set up mpl things
+    #mpl.use("TkAgg")
+    # kill the toolbar
+    mpl.rcParams['toolbar'] = 'None'
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import matplotlib.animation as animation
+
     ii = 0
-    while ii < iiMax:
-        if not pause:
-            ii += 1
-        yield ii
-
-pause = False
-def onClick(e):
-    timeAxes = [ax2, ax5, ax3, ax6]
-    if (e.inaxes in timeAxes):
+    def getIdx():
+        iiMax = numSteps
         global ii
-        dt = times[1] - times[0]
-        ii = int(e.xdata/dt)
-    else:
-        global pause
-        pause = not pause
-
-def onKey(e):
-    global ii
-    if (debug):
-        print("you pressed", e.key, e.xdata, e.ydata)
-    if (e.key == ' '):
-        global pause
-        pause = not pause
-    elif (e.key == 'left'):
         ii = 0
-    elif (e.key == 'right'):
-        ii = numSteps - 1
-    elif (e.key == 'q'):
-        import sys
-        sys.exit()
+        while ii < iiMax:
+            if not pause:
+                ii += 1
+            yield ii
 
-def animate(getIdx):
-    ii = getIdx
-    # update populations
-    for r,w in zip(kRects, kprobs[:,ii]):
-        r.set_width(w)
-    for r,w in zip(cRects, cprobs[:,ii]):
-        r.set_width(w)
-    # progress bars
-    arr2.set_positions((times[ii],min(Ek)), (times[ii],max(Ek)))
-    arr3.set_positions((times[ii],0), (times[ii],max(tkprob)))
-    arr5.set_positions((times[ii],min(Ec)), (times[ii],max(Ec)))
-    arr6.set_positions((times[ii],0), (times[ii],max(tcprob)))
-    return kRects, cRects, arr2, arr3, arr5, arr6
+    pause = False
+    def onClick(e):
+        timeAxes = [ax2, ax5, ax3, ax6]
+        if (e.inaxes in timeAxes):
+            global ii
+            dt = times[1] - times[0]
+            ii = int(e.xdata/dt)
+        else:
+            global pause
+            pause = not pause
 
-# read in data
-kprobs = np.loadtxt('outs/kprobs.out')
-cprobs = np.loadtxt('outs/cprobs.out')
-Ek = np.loadtxt('outs/energies.out')*27.211
-Ec = np.loadtxt('ins/c_energies.in')*27.211
-tkprob = np.loadtxt('outs/tkprob.out')[:,1]
-tcprob = np.loadtxt('outs/tcprob.out')[:,1]
+    def onKey(e):
+        global ii
+        if (debug):
+            print("you pressed", e.key, e.xdata, e.ydata)
+        if (e.key == ' '):
+            global pause
+            pause = not pause
+        elif (e.key == 'left'):
+            ii = 0
+        elif (e.key == 'right'):
+            ii = numSteps - 1
+        elif (e.key == 'q'):
+            import sys
+            sys.exit()
 
-# split kprobs into times and kprobs
-times = kprobs[:,0]
-kprobs = kprobs[:,1:].transpose()
-cprobs = cprobs[:,1:].transpose()
+    def animate(getIdx):
+        ii = getIdx
+        # update populations
+        for r,w in zip(kRects, kprobs[:,ii]):
+            r.set_width(w)
+        for r,w in zip(cRects, cprobs[:,ii]):
+            r.set_width(w)
+        # progress bars
+        arr2.set_positions((times[ii],min(Ek)), (times[ii],max(Ek)))
+        arr3.set_positions((times[ii],0), (times[ii],max(tkprob)))
+        arr5.set_positions((times[ii],min(Ec)), (times[ii],max(Ec)))
+        arr6.set_positions((times[ii],0), (times[ii],max(tcprob)))
+        return kRects, cRects, arr2, arr3, arr5, arr6
 
-Nk = kprobs.shape[0]
-Nc = cprobs.shape[0]
+    # read in data
+    kprobs = np.loadtxt('outs/kprobs.out')
+    cprobs = np.loadtxt('outs/cprobs.out')
+    Ek = np.loadtxt('outs/energies.out')*27.211
+    Ec = np.loadtxt('ins/c_energies.in')*27.211
+    tkprob = np.loadtxt('outs/tkprob.out')[:,1]
+    tcprob = np.loadtxt('outs/tcprob.out')[:,1]
 
-# account for k energies being at beginning of E array
-Ek = Ek[0:Nk]
+    # split kprobs into times and kprobs
+    times = kprobs[:,0]
+    kprobs = kprobs[:,1:].transpose()
+    cprobs = cprobs[:,1:].transpose()
 
-# set time steps
-if (debug):
-    numSteps = 25
-else:
-    numSteps = len(times)
+    Nk = kprobs.shape[0]
+    Nc = cprobs.shape[0]
 
-# set up contour plot parameters
-X1, Y1 = np.meshgrid(times, Ek)
-kLevels = np.linspace(kprobs.flatten().min(), kprobs.flatten().max(), 256)
+    # account for k energies being at beginning of E array
+    Ek = Ek[0:Nk]
 
-X2, Y2 = np.meshgrid(times, Ec)
-cLevels = np.linspace(cprobs.flatten().min(), cprobs.flatten().max(), 256)
+    # set time steps
+    if (debug):
+        numSteps = 25
+    else:
+        numSteps = len(times)
 
-# set up figure and axis opjects
-fig = plt.figure(figsize=(8,8))
-ax1 = plt.subplot2grid((3,2), (0,0))
-ax4 = plt.subplot2grid((3,2), (0,1))
-ax2 = plt.subplot2grid((3,2), (1,0))
-ax5 = plt.subplot2grid((3,2), (1,1))
-ax3 = plt.subplot2grid((3,2), (2,0))
-ax6 = plt.subplot2grid((3,2), (2,1))
-plt.set_cmap(plt.cm.coolwarm)
+    # set up contour plot parameters
+    X1, Y1 = np.meshgrid(times, Ek)
+    kLevels = np.linspace(kprobs.flatten().min(), kprobs.flatten().max(), 256)
 
-# population bar charts
-kRects = ax1.barh(Ek, kprobs[:,0], height=min([(Ek[ii+1]-Ek[ii]) for ii in range(Nk-1)]), color='r', ec='r')
-ax1.set_title('Populations on Donor')
-ax1.set_xlabel('Population (a.u.)')
-ax1.set_ylabel('Energy (eV)')
-ax1.set_xlim([0, kprobs.flatten().max()])
-ax1.set_ylim([min(Ek), max(Ek)])
+    X2, Y2 = np.meshgrid(times, Ec)
+    cLevels = np.linspace(cprobs.flatten().min(), cprobs.flatten().max(), 256)
 
-cRects = ax4.barh(Ec, cprobs[:,0], height=min([(Ec[ii+1]-Ec[ii]) for ii in range(Nc-1)]), color='g', ec='g')
-ax4.set_title('Populations on Acceptor')
-ax4.set_xlabel('Population (a.u.)')
-ax4.set_ylabel('Energy (eV)')
-ax4.set_xlim([0, cprobs.flatten().max()])
-ax4.set_ylim([min(Ec), max(Ec)])
+    # set up figure and axis opjects
+    fig = plt.figure(figsize=(8,8))
+    ax1 = plt.subplot2grid((3,2), (0,0))
+    ax4 = plt.subplot2grid((3,2), (0,1))
+    ax2 = plt.subplot2grid((3,2), (1,0))
+    ax5 = plt.subplot2grid((3,2), (1,1))
+    ax3 = plt.subplot2grid((3,2), (2,0))
+    ax6 = plt.subplot2grid((3,2), (2,1))
+    plt.set_cmap(plt.cm.coolwarm)
 
-# create and dress up contour plots
-ax2.contourf(X1, Y1, kprobs, origin='lower', levels=kLevels)
-ax2.set_xlabel('Time (fs)')
-ax2.set_ylabel('Energy (eV)')
-ax2.set_ylim([min(Ek), max(Ek)])
-arr2 = mpl.patches.FancyArrowPatch((0,min(Ek)),(0,max(Ek)))
-ax2.add_patch(arr2)
+    # population bar charts
+    kRects = ax1.barh(Ek, kprobs[:,0], height=min([(Ek[ii+1]-Ek[ii]) for ii in range(Nk-1)]), color='r', ec='r')
+    ax1.set_title('Populations on Donor')
+    ax1.set_xlabel('Population (a.u.)')
+    ax1.set_ylabel('Energy (eV)')
+    ax1.set_xlim([0, kprobs.flatten().max()])
+    ax1.set_ylim([min(Ek), max(Ek)])
 
-ax5.contourf(X2, Y2, cprobs, origin='lower', levels=cLevels)
-ax5.set_xlabel('Time (fs)')
-ax5.set_ylabel('Energy (eV)')
-ax5.set_ylim([min(Ec), max(Ec)])
-arr5 = mpl.patches.FancyArrowPatch((0,min(Ec)),(0,max(Ec)))
-ax5.add_patch(arr5)
+    cRects = ax4.barh(Ec, cprobs[:,0], height=min([(Ec[ii+1]-Ec[ii]) for ii in range(Nc-1)]), color='g', ec='g')
+    ax4.set_title('Populations on Acceptor')
+    ax4.set_xlabel('Population (a.u.)')
+    ax4.set_ylabel('Energy (eV)')
+    ax4.set_xlim([0, cprobs.flatten().max()])
+    ax4.set_ylim([min(Ec), max(Ec)])
 
-# plots of total population over time
-ax3.plot(times, tkprob)
-ax3.set_title('Total Population on Donor')
-ax3.set_xlabel('Time (fs)')
-ax3.set_ylabel('Population on Donor')
-ax3.set_ylim([0, max(tkprob)])
-ax3.set_xlim([0, max(times)])
-arr3 = mpl.patches.FancyArrowPatch((0,0), (0,max(tkprob)))
-ax3.add_patch(arr3)
+    # create and dress up contour plots
+    ax2.contourf(X1, Y1, kprobs, origin='lower', levels=kLevels)
+    ax2.set_xlabel('Time (fs)')
+    ax2.set_ylabel('Energy (eV)')
+    ax2.set_ylim([min(Ek), max(Ek)])
+    arr2 = mpl.patches.FancyArrowPatch((0,min(Ek)),(0,max(Ek)))
+    ax2.add_patch(arr2)
 
-ax6.plot(times, tcprob)
-ax6.set_title('Total Population on Acceptor')
-ax6.set_xlabel('Time (fs)')
-ax6.set_ylabel('Population on Donor')
-ax6.set_ylim([0, max(tcprob)])
-ax6.set_xlim([0, max(times)])
-arr6 = mpl.patches.FancyArrowPatch((0,0), (0,max(tcprob)))
-ax6.add_patch(arr6)
+    ax5.contourf(X2, Y2, cprobs, origin='lower', levels=cLevels)
+    ax5.set_xlabel('Time (fs)')
+    ax5.set_ylabel('Energy (eV)')
+    ax5.set_ylim([min(Ec), max(Ec)])
+    arr5 = mpl.patches.FancyArrowPatch((0,min(Ec)),(0,max(Ec)))
+    ax5.add_patch(arr5)
 
-# remove useless ticks
-noTickAxes = [ax2, ax5]
-[ax.tick_params(left='off', right='off', top='off', bottom='off') for ax in noTickAxes]
+    # plots of total population over time
+    ax3.plot(times, tkprob)
+    ax3.set_title('Total Population on Donor')
+    ax3.set_xlabel('Time (fs)')
+    ax3.set_ylabel('Population on Donor')
+    ax3.set_ylim([0, max(tkprob)])
+    ax3.set_xlim([0, max(times)])
+    arr3 = mpl.patches.FancyArrowPatch((0,0), (0,max(tkprob)))
+    ax3.add_patch(arr3)
 
-axes = [ax1, ax2, ax3, ax4, ax5, ax6]
-[ax.set_xticks(ax.get_xlim()) for ax in axes]
-[ax.set_yticks(ax.get_ylim()) for ax in axes]
+    ax6.plot(times, tcprob)
+    ax6.set_title('Total Population on Acceptor')
+    ax6.set_xlabel('Time (fs)')
+    ax6.set_ylabel('Population on Donor')
+    ax6.set_ylim([0, max(tcprob)])
+    ax6.set_xlim([0, max(times)])
+    arr6 = mpl.patches.FancyArrowPatch((0,0), (0,max(tcprob)))
+    ax6.add_patch(arr6)
 
-# link clicking to pause
-fig.canvas.mpl_connect('button_press_event', onClick)
+    # remove useless ticks
+    noTickAxes = [ax2, ax5]
+    [ax.tick_params(left='off', right='off', top='off', bottom='off') for ax in noTickAxes]
 
-# link space to pause
-fig.canvas.mpl_connect('key_press_event', onKey)
+    axes = [ax1, ax2, ax3, ax4, ax5, ax6]
+    [ax.set_xticks(ax.get_xlim()) for ax in axes]
+    [ax.set_yticks(ax.get_ylim()) for ax in axes]
 
-# create space for axis labels
-fig.set_tight_layout(True)
+    # link clicking to pause
+    fig.canvas.mpl_connect('button_press_event', onClick)
 
-anim = animation.FuncAnimation(fig, animate, getIdx, blit=False, interval=100,
-    repeat=True)
-#anim.save('anim2.mkv', fps=25, extra_args=['-vcodec', 'libx264', '-threads', '0'])
-plt.show()
+    # link space to pause
+    fig.canvas.mpl_connect('key_press_event', onKey)
+
+    # create space for axis labels
+    fig.set_tight_layout(True)
+
+    anim = animation.FuncAnimation(fig, animate, getIdx, blit=False, interval=100,
+        repeat=True)
+    #anim.save('anim2.mkv', fps=25, extra_args=['-vcodec', 'libx264', '-threads', '0'])
+    plt.show()
