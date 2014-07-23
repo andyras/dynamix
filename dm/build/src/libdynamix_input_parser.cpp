e21e2c42b7243a4d40784eccc352e78eaea123db
Andrew Rasmussen <andyras@gmail.com>
2014-07-23 12:37:16 -0500

    reversed input parser use of sto* functions, they are only in C++11 and icpc 14.0.1 does not support it.

--- dm/build/src/libdynamix_input_parser.cpp
+++ dm/build/src/libdynamix_input_parser.cpp
@@ -143,83 +143,156 @@ void assignParams(std::string inputFile, Params * p) {
     param_val = line.substr(int(equals_pos)+1,int(space_pos)-int(equals_pos));
     // extract parameters
 #ifdef DEBUG
-    std::cout << "Parameter: " << input_param << std::endl << "New value: " << stof(param_val.c_str()) << std::endl;
+    std::cout << "Parameter: " << input_param << std::endl << "New value: " << atof(param_val.c_str()) << std::endl;
 #endif
-    if (input_param == "timedepH") { p->timedepH = stoi(param_val); }
-    else if (input_param == "justPlots") { p->justPlots = stoi(param_val); }
-    else if (input_param == "nproc") { p->nproc = stoi(param_val); }
-    else if (input_param == "wavefunction") { p->wavefunction = stoi(param_val); }
-    else if (input_param == "coherent") { p->coherent = stoi(param_val); }
-    else if (input_param == "analytical") { p->analytical = stoi(param_val); }
-    else if (input_param == "kinetic") { p->kinetic = stoi(param_val); }
-    else if (input_param == "kineticQD") { p->kineticQD = stoi(param_val); }
-    else if (input_param == "dynamicMu") { p->dynamicMu = stoi(param_val); }
-    else if (input_param == "dynamicMuQD") { p->dynamicMuQD = stoi(param_val); }
-    else if (input_param == "rta") { p->rta = stoi(param_val); }
-    else if (input_param == "rtaQD") { p->rtaQD = stoi(param_val); }
-    else if (input_param == "dephasing") { p->dephasing = stoi(param_val); }
-    else if (input_param == "progressFile") { p->progressFile = stoi(param_val); }
-    else if (input_param == "progressStdout") { p->progressStdout = stoi(param_val); }
-    else if (input_param == "abstol") { p->abstol = stof(param_val); }
-    else if (input_param == "reltol" ) { p->reltol = stof(param_val); }
-    else if (input_param == "tout" ) { p->tout = stof(param_val); }
-    else if (input_param == "numsteps" ) { p->numsteps = stoi(param_val); }
-    else if (input_param == "numOutputSteps" ) { p->numOutputSteps = stoi(param_val); }
-    else if (input_param == "kBandEdge" ) { p->kBandEdge = stof(param_val); }
-    else if (input_param == "kBandTop" ) { p->kBandTop = stof(param_val); }
-    else if (input_param == "lBandTop" ) { p->lBandTop = stof(param_val); }
-    else if (input_param == "bulk_gap" ) { p->bulk_gap = stof(param_val); }
-    else if (input_param == "Nk" ) { p->Nk = stoi(param_val); }
-    else if (input_param == "Nk_first" ) { p->Nk_first = stoi(param_val); }
-    else if (input_param == "Nk_final" ) { p->Nk_final = stoi(param_val); }
-    else if (input_param == "Nc_first" ) { p->Nc_first = stoi(param_val); }
-    else if (input_param == "Nc_final" ) { p->Nc_final = stoi(param_val); }
-    else if (input_param == "valenceBand" ) { p->valenceBand = stof(param_val); }
-    else if (input_param == "Nl" ) { p->Nl = stoi(param_val); }
-    else if (input_param == "bulkGaussSigma" ) { p->bulkGaussSigma = stof(param_val); }
-    else if (input_param == "bulkGaussMu" ) { p->bulkGaussMu = stof(param_val); }
-    else if (input_param == "me" ) { p->me = stof(param_val); }
-    else if (input_param == "mh" ) { p->mh = stof(param_val); }
-    else if (input_param == "me_c" ) { p->me_c = stof(param_val); }
-    else if (input_param == "mh_c" ) { p->mh_c = stof(param_val); }
-    else if (input_param == "X2" ) { p->X2 = stof(param_val); }
-    else if (input_param == "temperature" ) { p->temperature = stof(param_val); }
-    else if (input_param == "EF" ) { p->EF = stof(param_val); }
-    else if (input_param == "gamma1" ) { p->gamma1 = stof(param_val); }
-    else if (input_param == "gamma1_c" ) { p->gamma1_c = stof(param_val); }
-    else if (input_param == "gamma2" ) { p->gamma2 = stof(param_val); }
-    else if (input_param == "muLK" ) { p->muLK = stof(param_val); }
-    else if (input_param == "pumpFWHM" ) { p->pumpFWHM = stof(param_val); }
-    else if (input_param == "pumpPeak" ) { p->pumpPeak = stof(param_val); }
-    else if (input_param == "pumpFreq" ) { p->pumpFreq = stof(param_val); }
-    else if (input_param == "pumpAmpl" ) { p->pumpAmpl = stof(param_val); }
-    else if (input_param == "pumpPhase" ) { p->pumpPhase = stof(param_val); }
-    else if (input_param == "CBPopFlag" ) { p->CBPopFlag = stoi(param_val); }
-    else if (input_param == "VBPopFlag" ) { p->VBPopFlag = stoi(param_val); }
-    else if (input_param == "QDPopFlag" ) { p->QDPopFlag = stoi(param_val); }
-    else if (input_param == "laser_on" ) { p->laser_on = stoi(param_val); }
-    else if (input_param == "parabolicCoupling" ) { p->parabolicCoupling = stoi(param_val); }
-    else if (input_param == "scale_bubr" ) { p->scale_bubr = stoi(param_val); }
-    else if (input_param == "scale_brqd" ) { p->scale_brqd = stoi(param_val); }
-    else if (input_param == "scale_buqd" ) { p->scale_buqd = stoi(param_val); }
-    else if (input_param == "scale_laser" ) { p->scale_laser = stoi(param_val); }
-    else if (input_param == "bridge_on" ) { p->bridge_on = stoi(param_val); }
-    else if (input_param == "random_phase" ) { p->random_phase = stoi(param_val); }
-    else if (input_param == "random_seed" ) { p->random_seed = stoi(param_val); }
-    else if (input_param == "torsion" ) { p->torsion = stoi(param_val); }
+    if (input_param == "timedepH") { p->timedepH = atoi(param_val.c_str()); }
+    else if (input_param == "justPlots") { p->justPlots = atoi(param_val.c_str()); }
+    else if (input_param == "nproc") { p->nproc = atoi(param_val.c_str()); }
+    else if (input_param == "wavefunction") { p->wavefunction = atoi(param_val.c_str()); }
+    else if (input_param == "coherent") { p->coherent = atoi(param_val.c_str()); }
+    else if (input_param == "analytical") { p->analytical = atoi(param_val.c_str()); }
+    else if (input_param == "kinetic") { p->kinetic = atoi(param_val.c_str()); }
+    else if (input_param == "kineticQD") { p->kineticQD = atoi(param_val.c_str()); }
+    else if (input_param == "dynamicMu") { p->dynamicMu = atoi(param_val.c_str()); }
+    else if (input_param == "dynamicMuQD") { p->dynamicMuQD = atoi(param_val.c_str()); }
+    else if (input_param == "rta") { p->rta = atoi(param_val.c_str()); }
+    else if (input_param == "rtaQD") { p->rtaQD = atoi(param_val.c_str()); }
+    else if (input_param == "dephasing") { p->dephasing = atoi(param_val.c_str()); }
+    else if (input_param == "progressFile") { p->progressFile = atoi(param_val.c_str()); }
+    else if (input_param == "progressStdout") { p->progressStdout = atoi(param_val.c_str()); }
+    else if (input_param == "abstol") { p->abstol = atof(param_val.c_str()); }
+    else if (input_param == "reltol" ) { p->reltol = atof(param_val.c_str()); }
+    else if (input_param == "tout" ) { p->tout = atof(param_val.c_str()); }
+    else if (input_param == "numsteps" ) { p->numsteps = atoi(param_val.c_str()); }
+    else if (input_param == "numOutputSteps" ) { p->numOutputSteps = atoi(param_val.c_str()); }
+    else if (input_param == "kBandEdge" ) { p->kBandEdge = atof(param_val.c_str()); }
+    else if (input_param == "kBandTop" ) { p->kBandTop = atof(param_val.c_str()); }
+    else if (input_param == "lBandTop" ) { p->lBandTop = atof(param_val.c_str()); }
+    else if (input_param == "bulk_gap" ) { p->bulk_gap = atof(param_val.c_str()); }
+    else if (input_param == "Nk" ) { p->Nk = atoi(param_val.c_str()); }
+    else if (input_param == "Nk_first" ) { p->Nk_first = atoi(param_val.c_str()); }
+    else if (input_param == "Nk_final" ) { p->Nk_final = atoi(param_val.c_str()); }
+    else if (input_param == "Nc_first" ) { p->Nc_first = atoi(param_val.c_str()); }
+    else if (input_param == "Nc_final" ) { p->Nc_final = atoi(param_val.c_str()); }
+    else if (input_param == "valenceBand" ) { p->valenceBand = atof(param_val.c_str()); }
+    else if (input_param == "Nl" ) { p->Nl = atoi(param_val.c_str()); }
+    else if (input_param == "bulkGaussSigma" ) { p->bulkGaussSigma = atof(param_val.c_str()); }
+    else if (input_param == "bulkGaussMu" ) { p->bulkGaussMu = atof(param_val.c_str()); }
+    else if (input_param == "me" ) { p->me = atof(param_val.c_str()); }
+    else if (input_param == "mh" ) { p->mh = atof(param_val.c_str()); }
+    else if (input_param == "me_c" ) { p->me_c = atof(param_val.c_str()); }
+    else if (input_param == "mh_c" ) { p->mh_c = atof(param_val.c_str()); }
+    else if (input_param == "X2" ) { p->X2 = atof(param_val.c_str()); }
+    else if (input_param == "temperature" ) { p->temperature = atof(param_val.c_str()); }
+    else if (input_param == "EF" ) { p->EF = atof(param_val.c_str()); }
+    else if (input_param == "gamma1" ) { p->gamma1 = atof(param_val.c_str()); }
+    else if (input_param == "gamma1_c" ) { p->gamma1_c = atof(param_val.c_str()); }
+    else if (input_param == "gamma2" ) { p->gamma2 = atof(param_val.c_str()); }
+    else if (input_param == "muLK" ) { p->muLK = atof(param_val.c_str()); }
+    else if (input_param == "pumpFWHM" ) { p->pumpFWHM = atof(param_val.c_str()); }
+    else if (input_param == "pumpPeak" ) { p->pumpPeak = atof(param_val.c_str()); }
+    else if (input_param == "pumpFreq" ) { p->pumpFreq = atof(param_val.c_str()); }
+    else if (input_param == "pumpAmpl" ) { p->pumpAmpl = atof(param_val.c_str()); }
+    else if (input_param == "pumpPhase" ) { p->pumpPhase = atof(param_val.c_str()); }
+    else if (input_param == "CBPopFlag" ) { p->CBPopFlag = atoi(param_val.c_str()); }
+    else if (input_param == "VBPopFlag" ) { p->VBPopFlag = atoi(param_val.c_str()); }
+    else if (input_param == "QDPopFlag" ) { p->QDPopFlag = atoi(param_val.c_str()); }
+    else if (input_param == "laser_on" ) { p->laser_on = atoi(param_val.c_str()); }
+    else if (input_param == "parabolicCoupling" ) { p->parabolicCoupling = atoi(param_val.c_str()); }
+    else if (input_param == "scale_bubr" ) { p->scale_bubr = atoi(param_val.c_str()); }
+    else if (input_param == "scale_brqd" ) { p->scale_brqd = atoi(param_val.c_str()); }
+    else if (input_param == "scale_buqd" ) { p->scale_buqd = atoi(param_val.c_str()); }
+    else if (input_param == "scale_laser" ) { p->scale_laser = atoi(param_val.c_str()); }
+    else if (input_param == "bridge_on" ) { p->bridge_on = atoi(param_val.c_str()); }
+    else if (input_param == "random_phase" ) { p->random_phase = atoi(param_val.c_str()); }
+    else if (input_param == "random_seed" ) { p->random_seed = atoi(param_val.c_str()); }
+    else if (input_param == "torsion" ) { p->torsion = atoi(param_val.c_str()); }
     else if (input_param == "torsionFile" ) { p->torsionFile = param_val; }
-    else if (input_param == "torsionSite" ) { p->torsionSite = stoi(param_val); }
-    else if (input_param == "torsionSin2" ) { p->torsionSin2 = stoi(param_val); }
-    else if (input_param == "torsionSin2V0" ) { p->torsionSin2V0 = stof(param_val); }
-    else if (input_param == "torsionSin2V1" ) { p->torsionSin2V1 = stof(param_val); }
-    else if (input_param == "torsionSin2omega" ) { p->torsionSin2omega = stof(param_val); }
-    else if (input_param == "torsionSin2phi" ) { p->torsionSin2phi = stof(param_val); }
+    else if (input_param == "torsionSite" ) { p->torsionSite = atoi(param_val.c_str()); }
+    else if (input_param == "torsionSin2" ) { p->torsionSin2 = atoi(param_val.c_str()); }
+    else if (input_param == "torsionSin2V0" ) { p->torsionSin2V0 = atof(param_val.c_str()); }
+    else if (input_param == "torsionSin2V1" ) { p->torsionSin2V1 = atof(param_val.c_str()); }
+    else if (input_param == "torsionSin2omega" ) { p->torsionSin2omega = atof(param_val.c_str()); }
+    else if (input_param == "torsionSin2phi" ) { p->torsionSin2phi = atof(param_val.c_str()); }
     else {  }
     getline (bash_in,line);
   }
 
   // close input file
   bash_in.close();
+
+#ifdef DEBUG
+  std::cout << std::endl;
+  std::cout << "justPlots is " << p->justPlots << std::endl;
+  std::cout << "timedepH is " << p->timedepH << std::endl;
+  std::cout << "nproc is " << p->nproc << std::endl;
+  std::cout << "wavefunction is " << p->wavefunction << std::endl;
+  std::cout << "coherent is " << p->coherent << std::endl;
+  std::cout << "analytical is " << p->analytical << std::endl;
+  std::cout << "kinetic is " << p->kinetic << std::endl;
+  std::cout << "kineticQD is " << p->kineticQD << std::endl;
+  std::cout << "dynamicMu is " << p->dynamicMu << std::endl;
+  std::cout << "dynamicMuQD is " << p->dynamicMuQD << std::endl;
+  std::cout << "rta is " << p->rta << std::endl;
+  std::cout << "rtaQD is " << p->rtaQD << std::endl;
+  std::cout << "dephasing is " << p->dephasing << std::endl;
+  std::cout << "progressFile is " << p->progressFile << std::endl;
+  std::cout << "progressStdout is " << p->progressStdout << std::endl;
+  std::cout << "abstol is " << p->abstol << std::endl;
+  std::cout << "reltol is " << p->reltol << std::endl;
+  std::cout << "tout is " << p->tout << std::endl;
+  std::cout << "numsteps is " << p->numsteps << std::endl;
+  std::cout << "numOutputSteps is " << p->numOutputSteps << std::endl;
+  std::cout << "kBandEdge is " << p->kBandEdge << std::endl;
+  std::cout << "kBandTop is " << p->kBandTop << std::endl;
+  std::cout << "lBandTop is " << p->lBandTop << std::endl;
+  std::cout << "bulk_gap is " << p->bulk_gap << std::endl;
+  std::cout << "Nk is " << p->Nk << std::endl;
+  std::cout << "valenceBand is " << p->valenceBand << std::endl;
+  std::cout << "Nl is " << p->Nl << std::endl;
+  std::cout << "bulkGaussSigma is " << p->bulkGaussSigma << std::endl;
+  std::cout << "bulkGaussMu is " << p->bulkGaussMu << std::endl;
+  std::cout << "me is " << p->me << std::endl;
+  std::cout << "mh is " << p->mh << std::endl;
+  std::cout << "me_c is " << p->me_c << std::endl;
+  std::cout << "mh_c is " << p->mh_c << std::endl;
+  std::cout << "X2 is " << p->X2 << std::endl;
+  std::cout << "temperature is " << p->temperature << std::endl;
+  std::cout << "EF is " << p->EF << std::endl;
+  std::cout << "gamma1 is " << p->gamma1 << std::endl;
+  std::cout << "gamma1_c is " << p->gamma1_c << std::endl;
+  std::cout << "gamma2 is " << p->gamma2 << std::endl;
+  std::cout << "muLK is " << p->muLK << std::endl;
+  std::cout << "pumpFWHM is " << p->pumpFWHM << std::endl;
+  std::cout << "pumpPeak is " << p->pumpPeak << std::endl;
+  std::cout << "pumpFreq is " << p->pumpFreq << std::endl;
+  std::cout << "pumpAmpl is " << p->pumpAmpl << std::endl;
+  std::cout << "pumpPhase is " << p->pumpPhase << std::endl;
+  std::cout << "CBPopFlag is " << p->CBPopFlag << std::endl;
+  std::cout << "VBPopFlag is " << p->VBPopFlag << std::endl;
+  std::cout << "QDPopFlag is " << p->QDPopFlag << std::endl;
+  std::cout << "Nk_first is " << p->Nk_first << std::endl;
+  std::cout << "Nk_final is " << p->Nk_final << std::endl;
+  std::cout << "Nc_first is " << p->Nc_first << std::endl;
+  std::cout << "Nc_final is " << p->Nc_final << std::endl;
+  std::cout << "laser_on is " << p->laser_on << std::endl;
+  std::cout << "parabolicCoupling is " << p->parabolicCoupling << std::endl;
+  std::cout << "scale_bubr is " << p->scale_bubr << std::endl;
+  std::cout << "scale_brqd is " << p->scale_brqd << std::endl;
+  std::cout << "scale_buqd is " << p->scale_buqd << std::endl;
+  std::cout << "scale_laser is " << p->scale_laser << std::endl;
+  std::cout << "bridge_on is " << p->bridge_on << std::endl;
+  std::cout << "random_phase is " << p->random_phase << std::endl;
+  std::cout << "random_seed is " << p->random_seed << std::endl;
+  std::cout << "torsion is " << p->torsion << std::endl;
+  std::cout << "torsionFile is " << p->torsionFile << std::endl;
+  std::cout << "torsionSite is " << p->torsionSite << std::endl;
+  std::cout << "torsionSin2 is " << p->torsionSin2 << std::endl;
+  std::cout << "torsionSin2V0 is " << p->torsionSin2V0 << std::endl;
+  std::cout << "torsionSin2V1 is " << p->torsionSin2V1 << std::endl;
+  std::cout << "torsionSin2omega is " << p->torsionSin2omega << std::endl;
+  std::cout << "torsionSin2phi is " << p->torsionSin2phi << std::endl;
+#endif
 
   // Error checking
   if ((p->VBPopFlag && p->CBPopFlag) || (p->VBPopFlag && p->QDPopFlag) || (p->CBPopFlag && p->QDPopFlag)) {
